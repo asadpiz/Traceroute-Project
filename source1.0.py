4904053c2e06a84e0533657167b19414c6db41be
Asad Syed <asadxflow@gmail.com>
2020-03-08 02:25:58 +0100

    Update 1.4

--- source1.0.py
+++ source1.0.py
@@ -1,76 +1,78 @@
-import subprocess, urllib2, re, simplekml
+import subprocess, urllib2, re, simplekml, os, time, whois
 
-####                ##      USER INPUT      ##
-####
-####print ("Enter Your Desired Public Traceroute Server")
-####choice = input ("1. Viafacil (Argentina)\n\
-####2. SPT (Australia)\n3.  Nemox.net (Austria)\n\
-####4. MegaLink (Bolivia)\n")
-##
-########EXTRACTING PUBLIC IP####
-####
-####publicip = urllib2.urlopen("http://automation.whatismyip.com/n09230945.asp").read()
-####print (publicip)
-##
-##
-########CREATING PUBLIC SERVER TR REQUEST URL & SELECTING it'S IP####
-####
-####if   (choice == 1):
-####    host = '200.123.164.101'
-####    URL = 'http://baires02.com.ar/traceroute/index.php?host=myIP&submit=Traceroute'
-####    URL = URL.replace ('myIP', publicip)
-####elif (choice == 2):
-####    host = '203.220.0.5'
-####    URL = 'http://mirror.sptel.com.au/cgi-bin/trace?myIP+'
-####    URL = URL.replace ('myIP', publicip)
-####elif (choice == 3):
-####    host = '83.137.41.254'
-####    URL = 'http://nemox.net/traceroute/index.pl?t=myIP'
-####    URL = URL.replace ('myIP', publicip)
-####elif (choice == 4):
-####    host = '200.75.160.7'
-####    URL = 'http://trace.megalink.com/index.php?host=myIP&submit=Traceroute!'
-####    URL = URL.replace ('myIP', publicip)
-####else:
-####    print ("You have Entered An Invalid Choice")
-##
-####    
-#################   CREATING FILES TO SAVE FORWARD & REVERSE TR ###############
-####
-####f1 = open ('trace'+str(choice)+'.txt','w')
-####f2 = open ('rTr'+str(choice)+'.txt', 'w')
-####
-####
-#################  PERFORMING REVERSER TRACEROUTE FROM PUBLIC SERVER   ##########
-####
-####print ("Performing REVERSE TRACEROUTE Please Wait...")
-####f2.write(urllib2.urlopen(URL).read())
-####
-####
-################# PERFROMING FORWARD TRACEROUTE FROM HOST MACHINE      ##########
-####
-####print ("Now Performing Forward Traceroute")
-####                ## tracert for WINDOWS|||||traceroute for LINUX
-####p = subprocess.Popen(["traceroute",host],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-####pa = 0
-####while True:
-####    line = p.stdout.readline()
-####    if not line: break
-####    print (line)
-####    f1.write(line)
-####p.wait()
-####
-####f1.close()
-####f2.close()
-##
-##
+                ##      USER INPUT      ##
+
+print ("Enter Your Desired Public Traceroute Server")
+choice = input ("1. Viafacil (Argentina)\n\
+2. SPT (Australia)\n3.  Nemox.net (Austria)\n\
+4. MegaLink (Bolivia)\n")
+            ## Making New Directory For Each Probe ##
+os.mkdir('probe'+str(choice))
+os.chdir ('probe'+str(choice))
+
+####EXTRACTING PUBLIC IP####
+start = time.time()
+#publicip = urllib2.urlopen("http://automation.whatismyip.com/n09230945.asp").read()
+publicip = '39.47.236.212'
+print (publicip)
+
+####CREATING PUBLIC SERVER TR REQUEST URL & SELECTING it'S IP####
+
+if   (choice == 1):
+    host = '200.123.164.101'
+    URL = 'http://baires02.com.ar/traceroute/index.php?host=myIP&submit=Traceroute'
+    URL = URL.replace ('myIP', publicip)
+elif (choice == 2):
+    host = '203.220.0.5'
+    URL = 'http://mirror.sptel.com.au/cgi-bin/trace?myIP+'
+    URL = URL.replace ('myIP', publicip)
+elif (choice == 3):
+    host = '83.137.41.254'
+    URL = 'http://nemox.net/traceroute/index.pl?t=myIP'
+    URL = URL.replace ('myIP', publicip)
+elif (choice == 4):
+    host = '200.75.160.7'
+    URL = 'http://trace.megalink.com/index.php?host=myIP&submit=Traceroute!'
+    URL = URL.replace ('myIP', publicip)
+else:
+    print ("You have Entered An Invalid Choice")
+
+    
+###########   CREATING FILES TO SAVE FORWARD & REVERSE TR ###############
+
+f1 = open ('trace'+str(choice)+'.txt','w')
+f2 = open ('rTr'+str(choice)+'.txt', 'w')
+
+
+###########  PERFORMING REVERSER TRACEROUTE FROM PUBLIC SERVER   ##########
+
+print ("Performing REVERSE TRACEROUTE Please Wait...")
+f2.write(urllib2.urlopen(URL).read())
+
+
+############# PERFROMING FORWARD TRACEROUTE FROM HOST MACHINE      ##########
+
+print ("Now Performing Forward Traceroute")
+                ## tracert for WINDOWS|||||traceroute for LINUX
+p = subprocess.Popen(["traceroute",host],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+pa = 0
+while True:
+    line = p.stdout.readline()
+    if not line: break
+    print (line)
+    f1.write(line)
+p.wait()
+
+f1.close()
+f2.close()
+
+
 ################# Extracting IPs & RTTs From REVERSE TRACEROUTE   #############
 
 RipS    = []
 HopFlag = []
 Rrttave = []
-##f3 = open ('rTr'+str(choice)+'.txt','r')
-f3 = open ('rTr4.txt','r')
+f3 = open ('rTr'+str(choice)+'.txt','r')
 Rtrfile = f3.read()
 
 
@@ -96,7 +98,7 @@ for line in Rregobj1.finditer (Rtrfile):
     for ip1 in Rregobj2.finditer (line.group()):
         IP1   = ip1.group()
         IP1   = IP1.strip('()')
-        print (IP1)
+        #print (IP1)
         if (first_ip == 0):
             RipS.append(IP1)
             first_ip = first_ip +1
@@ -114,7 +116,7 @@ for line in Rregobj1.finditer (Rtrfile):
     if (star!=1):
         for rtt1 in Rregobj4.finditer (line.group()):
             rtt11  = rtt1.group()
-            print (rtt1.group())
+            #print (rtt1.group())
             rtt11  = rtt11.strip (' ms')
             rtt11  = float (rtt11)
             length = length + 1
@@ -125,18 +127,17 @@ for line in Rregobj1.finditer (Rtrfile):
         
 f3.close()
 
-
+print ("I did the reverse Part")
 ################# Extracting IPs & RTTs From FORWARD TRACEROUTE   #############
 
 FipS    = []
 FHopFlag = []
 Frttave = []
 f4 = open ('trace'+str(choice)+'.txt','r')
-##f4 = open ('trace4.txt','r')
 Ftrfile = f4.read()
-#print (Ftrfile)
 
-linepattern = '((\d)+  .*)'
+#linepattern = '((\d)+  .*)'
+linepattern = '([ \n]\d+  .*)'
 ippattern   = '(\(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\))'
 starpattern = '(\d)+  \* \* \*'
 rttpattern  = '(\d*[.]\d* ms)'
@@ -187,51 +188,59 @@ for line in regularobj.finditer (Ftrfile):
 f4.close()
 
 
+Fgeo = open ('Fgeoip'+str(choice)+'.txt', 'w')
+Rgeo = open ('Rgeoip'+str(choice)+'.txt', 'w')
 
-##geo1 = open ('Fgeoip.txt', 'w')
-##### Parsing Forward IPs to GEOIPtool
-##for item in FipS:
-##        URL1 = 'http://www.geoiptool.com/en/?IP=IPADD'
-##        URL1 = URL1.replace ('IPADD', item)
-##        geo1.write (urllib2.urlopen(URL1).read())
-##        geo1.write ("\n\n---------NEXT IP-----------\n\n")
-##geo2 = open ('Rgeoip.txt', 'w')
-##### Parsing Reverse IPs to GEOIPtool
-##for item in RipS:
-##        URL2 = 'http://www.geoiptool.com/en/?IP=IPADD'
-##        URL2 = URL2.replace ('IPADD', item)
-##        geo2.write (urllib2.urlopen(URL2).read())
-##        geo2.write ("\n\n---------NEXT IP-----------\n\n")    
-##
-##geo1.close()
-##geo2.close()
-##
-##
-##Flonlat = []
-######                FINDING Forward LONGITUDE/LATITUDE              ####
-##web2 = open ('Fgeoip.txt', 'r')
-##text = web2.read()
-##bytePattern2 = '(>-?\d{1,3}<)|(>-?\d{1,3}[.]\d{0,4}<)'
-##regObj2 = re.compile(bytePattern2)
-##for same in regObj2.finditer(text):
-##    llat = same.group()
-##    llat = llat.strip ('><')
-##    print (llat)
-##    Flonlat.append(llat)
-##web2.close()
-##
-##Rlonlat = []
-######                FINDING Reverse LONGITUDE/LATITUDE              ####
-##web3 = open ('Rgeoip.txt', 'r')
-##text = web3.read()
-##bytePattern3 = '(>-?\d{1,3}<)|(>-?\d{1,3}[.]\d{0,4}<)'
-##regObj3 = re.compile(bytePattern3)
-##for same in regObj3.finditer(text):
-##    llat = same.group()
-##    llat = llat.strip ('><')
-##    print (llat)
-##    Rlonlat.append(llat)
-##web3.close()
+### Parsing FORWARD IPs to GEOIPtool  ##########
+
+for item in FipS:
+    if (item !=0):
+        URL1 = 'http://www.geoiptool.com/en/?IP=IPADD'
+        URL1 = URL1.replace ('IPADD', item)
+        Fgeo.write (urllib2.urlopen(URL1).read())
+        Fgeo.write ("\n\n---------NEXT IP-----------\n\n")
+
+
+### Parsing REVERSE IPs to GEOIPtool   ##########
+
+for item in RipS:
+    if (item!=0):
+        URL2 = 'http://www.geoiptool.com/en/?IP=IPADD'
+        URL2 = URL2.replace ('IPADD', item)
+        Rgeo.write (urllib2.urlopen(URL2).read())
+        Rgeo.write ("\n\n---------NEXT IP-----------\n\n")    
+
+Fgeo.close()
+Rgeo.close()
+
+
+##                FINDING Forward LONGITUDE/LATITUDE              ####
+
+Flonlat = []
+FLL = open ('Fgeoip'+str(choice)+'.txt', 'r')
+FLLtext = FLL.read()
+FLLpattern = '(>-?\d{1,3}<)|(>-?\d{1,3}[.]\d{0,4}<)'
+FLLregObj = re.compile(FLLpattern)
+for same in FLLregObj.finditer(FLLtext):
+    Fllat = same.group()
+    Fllat = Fllat.strip ('><')
+    print (Fllat)
+    Flonlat.append(Fllat)
+FLL.close()
+
+##                FINDING Reverse LONGITUDE/LATITUDE              ####
+Rlonlat = []
+RLL = open ('Rgeoip'+str(choice)+'.txt', 'r')
+RLLtext = RLL.read()
+RLLpattern = '(>-?\d{1,3}<)|(>-?\d{1,3}[.]\d{0,4}<)'
+RLLregObj = re.compile(RLLpattern)
+for same in RLLregObj.finditer(RLLtext):
+    Rllat = same.group()
+    Rllat = Rllat.strip ('><')
+    print (Rllat)
+    Rlonlat.append(Rllat)
+RLL.close()
+print "it took", time.time() - start, "seconds."
 ##
 ##
 ##
@@ -282,3 +291,4 @@ f4.close()
 ##n.coords = z
 ##kml2.save("Rroute1.kml")
 ##
+os.chdir(os.pardir)
